a8e43dd6c9f9f3398347e3f79f3a34c175e1661d
артем <hajnkof@gmail.com>
2023-09-27 16:10:53 +0300

    2

--- src/components/bestSellers/bestSellers.jsx
+++ src/components/bestSellers/bestSellers.jsx
@@ -7,25 +7,24 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
 import 'swiper/css/pagination';
+import { Link } from "react-router-dom";
+
 
 function BestSellers(props) {
     const productsArray = Object.values(Products);
-
-    // Filter and sort products by average rating
-    const bestRatedProducts = productsArray
-        .slice(0, 9); // Change the slice range to show elements from 0 to 9
-
     return (
         <div className='max-w-[1300px] mx-auto pl-4 pr-4'>
             <Swiper className='bg-white p'
                     navigation={true}
                     modules={[Navigation, Autoplay]}
                     slidesPerView='7' // Display one slide at a time
-                  spaceBetween={10}
+                    spaceBetween={10}
             >
-                {bestRatedProducts.map((product) => (
+                {productsArray.map((product) => (
                     <SwiperSlide key={product.id} className=''>
-                        <img src={require('../../' + product.image_small)} className='h-52' alt={product.title}/>
+                       <Link to={`/product/${product.id}`}>
+  <img src={require('../../' + product.image_small)} className='h-52' alt={product.title} />
+</Link>
                     </SwiperSlide>
                 ))}
             </Swiper>
--- src/pages/productPage.jsx
+++ src/pages/productPage.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { callAPI } from "../api/getProducts";
+import { useParams } from "react-router-dom";
 
 function ProductPage(props) {
-    return (
-        <div> product page</div>
-    );
+  const [product, setProduct] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    // Assuming callAPI retrieves a JSON array
+    callAPI('data/products.json').then((res) => {
+      // Find the product with the matching id
+      const foundProduct = res.find((item) => item.id === id);
+
+      if (foundProduct) {
+        setProduct(foundProduct);
+      } else {
+        // Handle the case where the product is not found
+        console.error(`Product with id ${id} not found.`);
+      }
+    });
+  }, [id]);
+
+  console.log(product);
+
+  return (
+    <div>
+      {product ? (
+        <div>
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          {/* Add more product details as needed */}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
 
 export default ProductPage;
