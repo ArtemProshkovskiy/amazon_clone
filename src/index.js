4192c2bea0f6a36d91a2c5e104fb60e5edaab3ff
artem <hajnkof@gmail.com>
2023-09-27 09:41:14 +0300

    22

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
         element: <MainPage></MainPage>
     },
     {
-        path: '/product',
+        path: '/product/:id',
         element: <ProductPage></ProductPage>
     }, {
 
--- src/pages/productPage.jsx
+++ src/pages/productPage.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import products from '../data/products.json';
 
-function ProductPage(props) {
+function ProductPage() {
+    const { id } = useParams();
+    const product = products[id]; // Access the product using the 'id' as the key
+
+    if (!product) {
+        return <div>Product not found</div>;
+    }
+
     return (
-        <div> product page</div>
+        <div>
+            <h2>{product.title}</h2>
+            <p>{product.description}</p>
+        </div>
     );
 }
 
